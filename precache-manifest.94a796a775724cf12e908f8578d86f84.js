a0ec91adf1cd8918c47d9e2d7937da20d1496613
JG <diasusb@hotmail.com>
2020-10-22 01:53:18 -0300

    Updates

--- precache-manifest.94a796a775724cf12e908f8578d86f84.js
+++ precache-manifest.94a796a775724cf12e908f8578d86f84.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ed08c47eb3b8b239b58f9e699b01a6e9",
+    "revision": "21f8628ae85e3c3641ef174e63b7dbaf",
     "url": "/fatiadev/index.html"
   },
   {
-    "revision": "16dbd232c36149d6c52a",
-    "url": "/fatiadev/static/css/main.0524b630.chunk.css"
+    "revision": "e9ed969ed216d701cb34",
+    "url": "/fatiadev/static/css/main.f56d4347.chunk.css"
   },
   {
     "revision": "3c90cdaed79c1f8c9e76",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/fatiadev/static/js/2.383854d8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "16dbd232c36149d6c52a",
-    "url": "/fatiadev/static/js/main.794caded.chunk.js"
+    "revision": "e9ed969ed216d701cb34",
+    "url": "/fatiadev/static/js/main.acc6a6e8.chunk.js"
   },
   {
     "revision": "fc26233b3376e75c6b7b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fatiadev/precache-manifest.e53bf7f161c7cd8ddf00f242e7f21d3a.js"
+  "/fatiadev/precache-manifest.94a796a775724cf12e908f8578d86f84.js"
 );
 
 self.addEventListener('message', (event) => {
